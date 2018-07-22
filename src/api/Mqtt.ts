1efffa8f06d139146a6bacd3029d6cbd975c9da7
FengChen <you@example.com>
2018-07-22 10:05:08 +0800

    项目初始化

--- src/api/Mqtt.ts
+++
@@ -1,5 +0,0 @@
-import MQTT from 'castle-mqtt'
-import config from '../config'
-import { UUID } from '../store/base/Login';
-const mqtt = new MQTT(config.MqttServer, config.AppName + '/', UUID)
-export default mqtt
--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export function hasPermission(PermissionName: string | number, tip: boolean = true, redirect: any = false) {
-    return true;
-    // let p = 'undefined' != typeof vuex.getters[G_USER_PERMISSIONS][PermissionName]
-    // if (!p && tip) { error('抱歉，您不具有该操作权限'); if (redirect) { router.push(redirect) } }
-    // return p;
-}
