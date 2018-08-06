6e0c468f7bea3320f8bc20135e21908e59edb3a7
FengChen <you@example.com>
2018-08-06 13:24:02 +0800

    ....

--- src/controller/User.ts
+++ src/controller/User.ts
@@ -2,16 +2,8 @@ import Controller from 'castle-koa/dist/lib/controller'
 //用户
 /**,
    //用户ID UID BIGINT ,
-   //用户账号 Count CHAR(250) ,
-   //用户密码 Password CHAR(250) ,
-   //用户昵称 Nick CHAR(50) ,
-   //用户性别 Sex TINYINT(1) ,
-   //用户年龄 Age INT ,
-   //用户电话 Phone CHAR(11) ,
-   //用户邮箱 Email CHAR(50) ,
-   //身份证号 IDCard CHAR(18) ,
-   //注册时间 CTime TIMESTAMP ,
-   //状态 Status TINYINT(1) ,
+   //用户账号 ID BIGINT ,
+   //用户密码 Password CHAR(8) ,
    */
 export default class User extends Controller{
 
@@ -32,23 +24,23 @@ export default class User extends Controller{
  */
     async login(post){
         //select UID,Password from User where Count = ?;
-        let out = await this.M('user').where({ID:post.ID}).fields('UID,Password').find();
-        if(out == null){
-            return{
-                "status":false,
-                "message":"账号不存在",
-            }
-        }
-        if(out.Password!=post.password){
-            return {
-                "status":false,
-                "message":"登录失败,密码错误",
-            }; 
-        }
-        return {
-            "status":true,
-            "UID":out.UID,
-            "message":"登录成功"
-        };   
+        // let out = await this.M('user').where({ID:post.ID}).fields('UID,Password').find();
+        // if(out == null){
+        //     return{
+        //         "status":false,
+        //         "message":"账号不存在",
+        //     }
+        // }
+        // if(out.Password!=post.password){
+        //     return {
+        //         "status":false,
+        //         "message":"登录失败,密码错误",
+        //     }; 
+        // }
+        // return {
+        //     "status":true,
+        //     "UID":out.UID,
+        //     "message":"登录成功"
+        // };   
     }
 }
--- src/db/User.ts
+++ src/db/User.ts
@@ -1,36 +1,36 @@
 import { DbDataType } from "castle-koa/dist/utils/iface";
 export default {
-    //用户ID 
-/**
+//     //用户ID 
+// /**
 
-*/
-    UID:{
-        type:DbDataType.bigint,
-        primaryKey:true,
-        autoIncrement:true,
-        defaultValue:'',
-        allowNull:false
-    },
-   //用户账号 
-/**
+// */
+//     UID:{
+//         type:DbDataType.bigint,
+//         primaryKey:true,
+//         autoIncrement:true,
+//         defaultValue:'',
+//         allowNull:false
+//     },
+//    //用户账号 
+// /**
 
-*/
-    ID:{
-        type:DbDataType.number,
-        primaryKey:false,
-        autoIncrement:false,
-        defaultValue:" ",
-        allowNull:false
-    },
-   //用户密码 
-/**
+// */
+//     ID:{
+//         type:DbDataType.bigint,
+//         primaryKey:false,
+//         autoIncrement:false,
+//         defaultValue:" ",
+//         allowNull:false
+//     },
+//    //用户密码 
+// /**
 
-*/
-    Password:{
-        type:DbDataType.char(8),
-        primaryKey:false,
-        autoIncrement:false,
-        defaultValue:" ",
-        allowNull:false
-    },
+// */
+//     Password:{
+//         type:DbDataType.char(8),
+//         primaryKey:false,
+//         autoIncrement:false,
+//         defaultValue:" ",
+//         allowNull:false
+//     },
 }
