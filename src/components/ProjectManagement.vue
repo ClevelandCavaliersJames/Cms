120c0042029df45c6b123ba9f50c3d6ce47f8d4c
woaifengchen <2211853499@qq.com>
2018-08-07 15:04:02 +0800

    项目管理

--- src/components/ProjectManagement.vue
+++ src/components/ProjectManagement.vue
@@ -1,9 +1,47 @@
 <template>
-    <Row>
-        <Col span="12">
-            <h2>项目管理</h2>
-        </Col>
-    </Row>
+    <div>
+        <Row>
+            <Col span="2">
+                <h2>项目管理</h2>
+            </Col>
+            <Col span="8">
+                <Table class="layui-table">
+                    <tr>
+                        <td colspan="4" style="text-align:center">以下需求由项目经理填写</td>
+                    </tr>
+                    <tr>
+                        <td class="td">项目经理</td>
+                        <td class="td"> <input v-model="a" style="width:100%;height:100%;padding:0px" /></td>
+                        <td>联系人电话</td>
+                        <td class="td"><input  v-model="b" style="width:100%;height:100%;padding:0px" /></td>
+                    </tr>
+                    <tr>
+                        <td>项目参与人员及负责人</td>
+                        <td>   
+                            <Form style="width:100px">
+                                <Select v-model="index" :options="name"  @change="selected(index)" >
+                                </Select>
+                            </Form>
+                        </td>
+                        <td>联系人电话</td>
+                        <td>{{phone[i]}}</td>
+                    </tr>
+                    <tr>
+                        <td>需求说明书</td>
+                        <td class="td" colspan="3"><Textarea  v-model="c" style="width:100%;height:100%;padding:0px"></Textarea ></td>
+                    </tr>
+                    <tr>
+                        <td>需求目标</td>
+                        <td class="td" colspan="3"><Textarea  v-model="d" style="width:100%;height:100%;padding:0px"></Textarea ></td>
+                    </tr>
+                    <tr>
+                        <td>项目里程碑</td>
+                        <td class="td" colspan="3"><Textarea  v-model="e" style="width:100%;height:100%;padding:0px"></Textarea ></td>
+                    </tr>
+                </Table>
+            </Col>
+        </Row>
+    </div>
 </template>
 
 <script lang = "ts">
@@ -13,7 +51,23 @@ import Component from "vue-class-component";
   props: {},
   components: {}
 })
-export default class ProjectManagement extends Vue {}
+export default class ProjectManagement extends Vue {
+    name:any=[
+        {text:'冯琛', value:'1'},
+        {text:'孙悟空',value:'2'},
+    ];
+    a:any='';
+    b:any='';
+    c:any='';
+    d:any='';
+    e:any='';
+    i:number=0;
+    index:any=''
+    phone:any=["","12345","09876"]
+    selected(v:any){
+       this.i=v
+    }
+}
 </script>
 
 <style scope>
