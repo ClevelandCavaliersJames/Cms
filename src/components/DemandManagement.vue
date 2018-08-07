2b7382ac137a6323809586e7a4b88f9155b64e73
woaifengchen <2211853499@qq.com>
2018-08-07 12:21:14 +0800

    需求管理界面

--- src/components/DemandManagement.vue
+++ src/components/DemandManagement.vue
@@ -1,9 +1,36 @@
 <template>
-    <Row>
-        <Col span="12">
-            <h2>需求管理</h2>
-        </Col>
-    </Row>
+    <div>
+        <Row>
+            <Col span="2">
+                <h2>需求管理</h2>
+            </Col>
+            <Col span="8">
+                <Table class="layui-table">
+                    <tr>
+                        <td colspan="4" style="text-align:center">以下需求由提出人填写</td>
+                    </tr>
+                    <tr>
+                        <td class="td">需求提出人</td>
+                        <td class="td"> <input v-model="a" style="width:100%;height:100%;padding:0px" /></td>
+                        <td>联系人电话</td>
+                        <td class="td"><input  v-model="b" style="width:100%;height:100%;padding:0px" /></td>
+                    </tr>
+                    <tr>
+                        <td>需求原因/目的</td>
+                        <td class="td" colspan="3"><Textarea  v-model="a" style="width:100%;height:100%;padding:0px"></Textarea ></td>
+                    </tr>
+                    <tr>
+                        <td>需求描述</td>
+                        <td class="td" colspan="3"><Textarea  v-model="b" style="width:100%;height:100%;padding:0px"></Textarea ></td>
+                    </tr>
+                    <tr>
+                        <td>期望上线日期</td>
+                        <td colspan="3"><input  v-model="c" style="width:100%;height:100%;padding:0px" /></td>
+                    </tr>
+                </Table>
+            </Col>
+        </Row>
+    </div>
 </template>
 
 <script lang = "ts">
@@ -13,8 +40,14 @@ import Component from "vue-class-component";
   props: {},
   components: {}
 })
-export default class DemandManagement extends Vue {}
+export default class DemandManagement extends Vue {
+    asd:any="";
+}
 </script>
 
 <style scope>
+.td{
+    margin:0px;
+    padding:0px;
+}
 </style>
