abb4d7925f077d89fbf982d262d0f06b4a66442a
woaifengchen <2211853499@qq.com>
2018-08-09 11:20:28 +0800

    存在问题的UI

---
+++ src/components/Modal/LoginUI.vue
@@ -0,0 +1,38 @@
+<template>
+    <Modal v-model="ShowModal" title=".." :options="options">
+        Modal内容
+    </Modal>
+</template>
+
+<script  lang="ts">
+import Vue from "../../vue";
+import Component from "vue-class-component";
+@Component({
+  props: {
+    value: {
+        type: Boolean,
+        Boolean:false
+    }
+  }
+})
+export default class LoginUI extends Vue {
+    value:any;
+    options:object={
+        area:"70%",
+        anim:1,
+        maxmin:true,
+        moveOut:true,
+        btn:['确定'],
+    };
+    get ShowModal(){
+        return this.value;
+    }
+    set ShowModal(v:Boolean){
+        this.$emit("input",v)
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
