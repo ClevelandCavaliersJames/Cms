ee7308d216b5ee85c5456db5b5160fcf25657c1f
FengChen <f@tansunyun.cn>
2018-08-11 11:40:06 +0800

    完成

--- dist/controller/Demand.js
+++ dist/controller/Demand.js
@@ -14,6 +14,7 @@ class Demand extends controller_1.default {
         return __awaiter(this, void 0, void 0, function* () {
             const add = yield this.M("demand").add({ Maker: post.Maker, DeterminingP: post.DeterminingP, PutTime: post.PutTime, ConfirmationTime: post.ConfirmationTime, DemanUser: post.DemanUser, Importance: post.Importance, ProjectCycle: post.ProjectCycle, PlannedTime: post.PlannedTime, ProjectLeader: post.ProjectLeader, DemandLeader: post.DemandLeader, DemandID: post.DemandID, Descriptive: post.Descriptive, Technological: post.Technological, Acceptance: post.Acceptance, Enclosure: post.Enclosure, Scheme: post.Scheme, Difficulties: post.Difficulties, Precondition: post.Precondition });
             console.log(add);
+            return add;
         });
     }
 }
--- src/controller/Demand.ts
+++ src/controller/Demand.ts
@@ -4,5 +4,6 @@ export default class Demand extends Controller{
     async addDemand(post){
         const add = await this.M("demand").add({Maker:post.Maker,DeterminingP:post.DeterminingP,PutTime:post.PutTime,ConfirmationTime:post.ConfirmationTime,DemanUser:post.DemanUser,Importance:post.Importance,ProjectCycle:post.ProjectCycle,PlannedTime:post.PlannedTime,ProjectLeader:post.ProjectLeader,DemandLeader:post.DemandLeader,DemandID:post.DemandID,Descriptive:post.Descriptive,Technological:post.Technological,Acceptance:post.Acceptance,Enclosure:post.Enclosure,Scheme:post.Scheme,Difficulties:post.Difficulties,Precondition:post.Precondition})
         console.log(add)
+        return add
         }
 }
